cde6de87a8e2419ec79215d1bdc2599616be263f
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-22 16:49:37 +0100

    restore old dates of the array contacts

--- js/script.js
+++ js/script.js
@@ -18,17 +18,17 @@ createApp({
                     dating: '',
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
                         }
@@ -40,17 +40,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                         }
@@ -62,17 +62,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
                         }
@@ -84,12 +84,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                         }
@@ -101,12 +101,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
                         }
@@ -118,17 +118,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
                         }
@@ -140,12 +140,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
                         }
@@ -157,17 +157,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                         },
                         {
-                        date: `${this.currentDate()}`,
+                        date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received'
                         }
